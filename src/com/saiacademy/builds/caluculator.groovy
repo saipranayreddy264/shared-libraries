47339c0ea22de2964bd451432942e03d3400efc0
saipranayreddy264 <78652475+saipranayreddy264@users.noreply.github.com>
2025-12-14 19:18:43 -0600

    added code

---
+++ src/com/saiacademy/builds/caluculator.groovy
@@ -0,0 +1,15 @@
+package com.saiacademy.builds
+
+//defining a class
+class Calculator {
+    def jenkins
+
+    //constucter
+    Calculator(jenkins){
+        this.jenkins = jenkins
+    }
+
+    def add(firstNumber + secondNumber) {
+        return firstNumber + secondNumber
+    }
+}
---
+++ vars/Firstpipeline.groovy
@@ -0,0 +1,43 @@
+import com.saiacademy.builds.Calculator
+def call(pipelineParams) {
+   Calculator calc = new Calculator(this)
+   pipeline {
+    agent {
+        label 'java-slave'
+    }
+    environment {
+        // Define your environment variables here
+        APPLICATION_NAME = "${pipelineParams.appName}"
+    }   
+
+    stages {
+        stage('addition') {
+            steps {
+                script {
+                    echo "The addition of two numbers is: ${calc.add(10,20)}")
+                println calc.add(2,3)
+                }
+            }
+            
+        }
+        stage('Build') {
+            steps {
+                echo Building the ${env.APPLICATION_NAME}
+                // Add your build steps here
+            }
+        }
+        stage('Test') {
+            steps {
+                echo  Testing the ${env.APPLICATION_NAME}
+                // Add your test steps here
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo Deploying the ${env.APPLICATION_NAME}
+                // Add your deploy steps here
+            }
+        }
+    }
+   } 
+}
